23a5a4280b50b7b7709542f13e5e23515fe3dbb2
Sohaib samad <115487764+samAd0san@users.noreply.github.com>
2024-10-31 17:42:04 +0530

    Create VowelCounter.js

---
+++ howdy/src/com/Fibonacci/VowelCounter.js
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class VowelCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+
+        int vowelCount = countVowels(inputString);
+
+        System.out.println("The number of vowels in the string is: " + vowelCount);
+    }
+
+    public static int countVowels(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = Character.toLowerCase(str.charAt(i));
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
